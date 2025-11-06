bd442b499269aa287f3ad47eca3f7c9a25dd9f85
Kevin Bosirany ORLANDO <kevinbosirany@gmail.com>
2025-11-06 20:40:46 +0100

    feat: update render_reports function to accept additional arguments

--- R/render_reports.R
+++ R/render_reports.R
@@ -1,5 +1,7 @@
 #' @export
 #'
-render_reports <- function() {
-  fairify::render_reports(clean_cache = TRUE, allow_duplicate_labels = TRUE)
+render_reports <- function(...) {
+  fairify::render_reports(
+    ..., clean_cache = TRUE, allow_duplicate_labels = TRUE
+  )
 }
